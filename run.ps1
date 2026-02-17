1015a44f87f9217fd4e8e0881853c698e45db3d2
Pawel Galezowski <pgalezowski@gmail.com>
2026-02-17 22:42:25 +0200

    wor run

--- run.ps1
+++ run.ps1
@@ -2,4 +2,4 @@
 # ..\Chat\Bin\revuo-chat.exe workflow -f C:\Work\Sandbox\RSDK\workflow.json -o
 
 # open gui app
-C:\Work\Sandbox\Chat\Src\Revuo.Chat.Client\bin\Debug\net10.0-windows10.0.19041.0\win-x64\Revuo.Chat.Client.exe workflow C:\\Work\\Sandbox\\RSDK\\InstallationRequest.json
+C:\Work\Sandbox\Chat\Src\Revuo.Chat.Client\bin\Debug\net10.0-windows10.0.19041.0\win-x64\Revuo.Chat.Client.exe workflow C:\\Work\\Sandbox\\RSDK\\InstallRSDKWorkflow.json
