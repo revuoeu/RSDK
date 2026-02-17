d59b9850aeff6f0d3e63afcdf5deaff42d8cd49a
Pawel Galezowski <pgalezowski@gmail.com>
2026-02-17 22:21:23 +0200

    wrokign debug mode

--- run.ps1
+++ run.ps1
@@ -1,2 +1,4 @@
 # ..\Chat\Bin\revuo-chat.exe run -ic -a Installer.Client.InstallerApp -act TemporaryInstallFromDll -pf C:\Work\Sandbox\RSDK\InstallationRequest.json
-..\Chat\Bin\revuo-chat.exe workflow -f C:\Work\Sandbox\RSDK\workflow.json -o
+# ..\Chat\Bin\revuo-chat.exe workflow -f C:\Work\Sandbox\RSDK\workflow.json -o
+
+C:\Work\Sandbox\Chat\Src\Revuo.Chat.Client\bin\Debug\net10.0-windows10.0.19041.0\win-x64\Revuo.Chat.Client.exe workflow C:\\Work\\Sandbox\\RSDK\\workflow.json
